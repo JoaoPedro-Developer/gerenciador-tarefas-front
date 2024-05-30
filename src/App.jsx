a1508b451fdbbd1d252791f0a782aaee69aff5bd
JoaoPedro-Developer <126725562+JoaoPedro-Developer@users.noreply.github.com>
2024-05-30 20:24:38 -0300

    feat: add app container

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import SideBar from './components/sideBar';
 import Tasks from './components/tasks'
+import SideBar from './components/sideBar'
 
+import './App.scss'
 const App = () => {
-  return <>
+  return <div className='app-container'>
     <SideBar/>
     <Tasks/>
-  </>
+  </div>
 }
 
 export default App;
